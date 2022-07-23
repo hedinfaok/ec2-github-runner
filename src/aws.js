afbc2767d7fa81628ef99792b4ba53924e253344
Rob Eden <rob.eden@fairygodboss.com>
2022-07-23 11:31:16 -0400

    pff

--- src/aws.js
+++ src/aws.js
@@ -3,6 +3,21 @@ const core = require('@actions/core');
 const config = require('./config');
 const merge = require('deepmerge');
 
+function combineMerge(target, source, options) {
+  const destination = target.slice();
+
+  source.forEach((item, index) => {
+    if (typeof destination[index] === 'undefined') {
+      destination[index] = options.cloneUnlessOtherwiseSpecified(item, options);
+    } else if (options.isMergeableObject(item)) {
+      destination[index] = merge(target[index], item, options);
+    } else if (target.indexOf(item) === -1) {
+      destination.push(item);
+    }
+  });
+  return destination;
+}
+
 // User data scripts are run as the root user
 function buildUserDataScript(githubRegistrationToken, label) {
   if (config.input.runnerHomeDir) {
